618c72943e0eab58508c4fff4bd1a1b4dc5e9f5b
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-10-27 15:57:40 +0530

    Introduced patternsUtilLibTest.js. Tested utility functions.

---
+++ test/patternsUtilLibTest.js
@@ -0,0 +1,27 @@
+const {generateLine, generateEmptyLine} = require('../src/patternsUtilLib.js');
+const assert = require('assert');
+
+//Test generateLine
+const generateLineTest = function() {
+  let expectedOutput = "";
+  expectedOutput = "****"
+  assert.equal(generateLine(4,"*"), expectedOutput);
+  expectedOutput = "********"
+  assert.equal(generateLine(8,"*"), expectedOutput);
+  expectedOutput = ""
+  assert.equal(generateLine(0,"*"), expectedOutput);
+}
+
+const generateEmptyLineTest = function() {
+  let expectedOutput = "";
+  expectedOutput = "*  *"
+  assert.equal(generateEmptyLine(4), expectedOutput);
+  expectedOutput = "*      *";
+  assert.equal(generateEmptyLine(8), expectedOutput);
+  expectedOutput = "**";
+  assert.equal(generateEmptyLine(2), expectedOutput);
+}
+
+generateLineTest();
+generateEmptyLineTest();
+console.log("All Tests passed...")
